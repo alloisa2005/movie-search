ef0c833641df03b66bce37a9eb5335846fe3657e
Anthony Allois <anthonyallois@gmail.com>
2024-02-22 16:26:41 -0300

    Ajustes en Movie Detail Page

--- app/movies/[id]/page.jsx
+++ app/movies/[id]/page.jsx
@@ -7,24 +7,27 @@ const MovieDetail = async ({ params }) => {
   const { id } = params;
   const movie = await getMovieById(id);
   console.log(movie);
+  // src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} 
 
   return (
-    <section className='absolute top-0 -z-10 w-full h-[460px]'>
-
-      <Image 
-        src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}                        
-        priority
-        width={1920}         
-        height={400}
-        className='w-full h-full object-cover brightness-[60%]'
-      />
-
-      <div className=" absolute left-0 bottom-4 w-full">
-        <div className='contenedor'>
-          <h1 className='text-gray-200 tracking-widest'>{movie.title}</h1>
+    <section className='absolute top-0 left-0 w-full -z-10'>
+      <div className='w-full h-[500px] relative'>
+        <Image 
+          src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+          alt={movie.title}
+          width={1920}
+          height={1080}
+          priority
+          className='w-full h-[500px] absolute object-cover'
+        />   
+        <div className='absolute w-full bottom-12 left-0'>
+          <h1 className='text-5xl text-white tracking-wider contenedor'>{movie.title}</h1>
         </div>
       </div>
       
+      <div className='contenedor'>
+        <p>{movie.overview}</p>
+      </div>
     </section>
   )
 }
